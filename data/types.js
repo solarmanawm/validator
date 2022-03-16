462117c2ba897436200e631c728886e3bc4bea0d
Dmitry Fisunov <solarman.awm@gmail.com>
2022-03-16 16:24:18 +0300

    Add a min length handler

--- data/types.js
+++ data/types.js
@@ -1,10 +1,11 @@
 /**
- * @type {{pattern: string, email: string, required: string}}
+ * @type {{minLength: string, pattern: string, email: string, required: string}}
  */
 const HANDLER_TYPES = {
     email: 'email',
     required: 'required',
     pattern: 'pattern',
+    minLength: 'minLength',
 };
 
 export default HANDLER_TYPES;
